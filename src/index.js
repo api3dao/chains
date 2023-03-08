d75ed62579dc32a90a8fb7852bdb1da5d95fdac3
bbenligiray <bbenligiray@gmail.com>
2023-03-08 13:50:22 +0300

    Add hardhat-etherscan support

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ const { getChains } = require("./chains");
 
 module.exports = {
   hardhatConfigNetworks: () => {
-    const chains = getChains();
-    return chains.reduce((networks, chain) => {
+    return getChains().reduce((networks, chain) => {
       networks[chain.alias] = {
         accounts: {
           mnemonic: "",
@@ -13,5 +12,29 @@ module.exports = {
       };
       return networks;
     }, {});
+  },
+  hardhatEtherscan: () => {
+    return getChains().reduce(
+      (etherscan, chain) => {
+        if (chain.explorer.api) {
+          etherscan.customChains.push({
+            network: chain.alias,
+            chainId: chain.id,
+            urls: {
+              apiURL: chain.explorer.api.url,
+              browserURL: chain.explorer.browserUrl,
+            },
+          });
+          if (chain.explorer.api.requiresKey) {
+            etherscan.apiKey[chain.alias] = "";
+          }
+        }
+        return etherscan;
+      },
+      {
+        apiKey: {},
+        customChains: [],
+      }
+    );
   },
 };
