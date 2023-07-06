7d64008715fb3ae1f0451c85dc1811e2c0dffd76
bbenligiray <bbenligiray@gmail.com>
2023-07-06 16:00:10 +0300

    Fix toUpperSnakeCase() to have the tests pass

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -1,8 +1,8 @@
 export function toUpperSnakeCase(input: string): string {
   return input
+    .replace(/[^a-zA-Z0-9\s]+/g, ' ') // replace each non-alphanumeric character with a space
+    .replace(/\s+/g, ' ') // replace consecutive spaces with a single space
     .trim()
-    .replace(/\s+/g, ' ') // replace multiple spaces with a single space
-    .replace(/[^a-zA-Z0-9\s]+/g, '') // remove all non-alphanumeric characters
     .split(' ')
     .join('_')
     .toUpperCase();
