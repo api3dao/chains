f98c1a7b4ac3daedb7256a1a357257b921185aff
André Ogle <ogleandre@gmail.com>
2023-09-12 11:46:14 +0200

    Rework ping-providers script to ping Slack and run on a CI cron schedule

--- scripts/ping-providers.ts
+++ scripts/ping-providers.ts
@@ -1,21 +1,61 @@
-import { JsonRpcProvider } from 'ethers';
+import { WebClient } from '@slack/web-api';
+import { createPublicClient, http } from 'viem';
 import { CHAINS } from '../src';
 
 const specifiedChain = CHAINS.find((chain) => chain.alias === process.env.CHAIN);
 const chains = specifiedChain ? [specifiedChain] : CHAINS;
 
-chains.forEach(async (chain) => {
-  const provider = new JsonRpcProvider(chain.providerUrl);
-  const chainId = (await provider.getNetwork()).chainId;
-  if (chainId.toString() !== chain.id) {
-    throw new Error(`${chain.alias} provider reports chain ID to be ${chainId}, while it is defined to be ${chain.id}`);
+// Set the following environment variables to also notify to Slack
+const slackToken = process.env.SLACK_TOKEN;
+const slackChannel = process.env.SLACK_CHANNEL;
+const slackClient = new WebClient(slackToken);
+
+async function pingAll(): Promise<PromiseSettledResult<void>[]> {
+  const promises = chains.map(async (chain) => {
+    const client = createPublicClient({ transport: http(chain.providerUrl) });
+
+    const chainId = await client.getChainId();
+    if (chainId.toString() !== chain.id) {
+      throw new Error(`${chain.alias} provider reports chain ID as ${chainId}, while it is defined as ${chain.id}`);
+    }
+
+    const block = await client.getBlock({ blockTag: 'latest' });
+    const blockTimestamp = block.timestamp;
+    const deltaTime = Number(BigInt(Math.floor(new Date().getTime() / 1000)) - blockTimestamp);
+    const tolerance = 5 * 60; // 5 minutes
+    if (Math.abs(deltaTime) > tolerance) {
+      throw new Error(`${chain.alias} latest block timestamp is ${deltaTime} seconds behind the system clock`);
+    }
+  });
+
+  return await Promise.allSettled(promises);
+}
+
+async function notifySlack(errors: Error[]): Promise<Error[]> {
+  if (errors.length > 0 && slackChannel) {
+    const text = errors.reduce((acc, error) => `${acc}\n${error.message}`, '');
+    await slackClient.chat.postMessage({ channel: slackChannel, text });
+    return errors;
   }
-  const blockTimestamp = (await provider.getBlock('latest'))!.timestamp;
-  const deltaTime = Math.floor(new Date().getTime() / 1000) - blockTimestamp;
-  const tolerance = 5 * 60;
-  if (Math.abs(deltaTime) > tolerance) {
-    throw new Error(
-      `Timestamp of the latest block the ${chain.alias} provider reports is ${deltaTime} seconds behind the system clock`
-    );
-  }
-});
+  return errors;
+}
+
+pingAll()
+  .then((results) => {
+    const rejectedPromises = results.filter((res) => res.status === 'rejected') as PromiseRejectedResult[];
+    const errors = rejectedPromises.map((res) => res.reason) as Error[];
+    return notifySlack(errors);
+  })
+  .then((errors) => {
+    if (errors.length > 0) {
+      errors.forEach((error) => {
+        console.log(error.message);
+      });
+      throw new Error('At least one chain did not pass all provider checks');
+    }
+  })
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
