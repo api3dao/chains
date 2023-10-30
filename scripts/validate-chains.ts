9ef724dac85db4cec5350e5133c0a5c5a9817be3
André Ogle <ogleandre@gmail.com>
2023-10-30 17:12:07 +0000

    Log all validation errors at once

--- scripts/validate-chains.ts
+++ scripts/validate-chains.ts
@@ -14,21 +14,20 @@ const jsonChains: any[] = jsonFiles.map((filePath: string) => {
   return JSON.parse(fileContentRaw);
 });
 
+const logs: string[] = [];
+
 // Validation: Ensure that each JSON file is represented in the CHAINS array
 if (CHAINS.length !== jsonChains.length) {
-  console.log('Generated chains differs in length to the number of JSON files');
-  console.log(`Generated CHAINS length = ${CHAINS.length}. Expected ${jsonChains.length} chains`);
-  console.log('Try regenerating chains');
-  process.exit(1);
+  logs.push('Generated chains differs in length to the number of JSON files');
+  logs.push(`Generated CHAINS length = ${CHAINS.length}. Expected ${jsonChains.length} chains\n`);
 }
 
 // Validation: Ensure that each JSON file is named using the chain's alias
 jsonFiles.forEach((filePath: string, index: number) => {
   const chain = jsonChains[index]!;
   if (filePath.replace('.json', '') !== chain.alias) {
-    console.log("JSON file name must match the chain's alias");
-    console.log(`Current value: ${filePath}.json. Expected: ${chain.alias}.json`);
-    process.exit(1);
+    logs.push("JSON file name must match the chain's alias");
+    logs.push(`Current value: ${filePath}.json. Expected: ${chain.alias}.json\n`);
   }
 });
 
@@ -39,18 +38,21 @@ jsonChains.forEach((chain: any, index: number) => {
     const errors = res.error.issues.map((issue) => {
       return `  path: '${issue.path.join('.')}' => '${issue.message}' `;
     });
-    console.log(`Chain '${chain.name}' contains the following errors:\n${errors.join('\n')}\n`);
-    process.exit(1);
+    logs.push(`Chain '${chain.name}' contains the following errors:\n${errors.join('\n')}\n`);
   }
 
   // Validation: Ensure that the latest JSON content is represented in each Chain object
   const existingChain = CHAINS[index];
   if (!deepEqual(chain, existingChain)) {
-    console.log(`Chain '${chain.name}' differs to the currently generated Chain object in CHAINS`);
-    console.log('Try regenerating chains');
-    process.exit(1);
+    logs.push(`Chain '${chain.name}' differs to the currently generated Chain object in CHAINS\n`);
   }
 });
+
+if (logs.length > 0) {
+  logs.forEach((log) => console.log(log));
+  console.log('NOTE: You will need to regenerate chains');
+  process.exit(1);
+}
 
 console.log('Successfully validated chains!');
 process.exit(0);
