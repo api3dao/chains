e6d23764274f673dd65573ec997b1bad620c5559
hiletmis <31757771+hiletmis@users.noreply.github.com>
2024-12-17 12:51:32 +0300

    Add alchemy provider to Sonic chain configuration

--- src/generated/chains.ts
+++ src/generated/chains.ts
@@ -1618,7 +1618,10 @@ export const CHAINS: Chain[] = [
     explorer: { browserUrl: 'https://explorer.soniclabs.com/' },
     id: '146',
     name: 'Sonic',
-    providers: [{ alias: 'default', rpcUrl: 'https://rpc.sonic.soniclabs.com' }],
+    providers: [
+      { alias: 'default', rpcUrl: 'https://rpc.sonic.soniclabs.com' },
+      { alias: 'alchemy', homepageUrl: 'https://alchemy.com' },
+    ],
     symbol: 'S',
     testnet: false,
   },
